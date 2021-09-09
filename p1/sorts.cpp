4efa1856a3e807d5479d263146f0b5c223e8d197
yaboiksa <ky.samuel1@gmail.com>
2021-09-08 20:21:37 -0400

    quicksort done, not tested

--- p1/sorts.cpp
+++ p1/sorts.cpp
@@ -4,6 +4,42 @@ void sorts::insertion_sort(int* arr, ull size) {
 
 }
 
-void sorts::quick_sort(int* arr, ull size) {
-    
+void swap(int* arr, ull item1, ull item2) {
+    int temp = arr[item1];
+    arr[item1] = arr[item2];
+    arr[item2] = temp;
 }
+
+ull partition(int* arr, ull size , ull first, ull last) {
+    int pivot = arr[last];
+    ull lower = first;
+    ull upper = last-1;
+
+    while (lower <= upper) {
+            while (lower <= upper && arr[upper] >= pivot)
+                upper--;
+            while (lower <= upper && arr[lower] <= pivot)
+                lower++;
+            if (lower < upper)
+                swap(arr, lower, upper);
+            
+    }
+    swap(arr, lower, last);
+    return lower;
+}
+
+void quickSort(int* arr, ull size, ull first, ull last){
+    if (first < last) {
+        ull mid = partition(arr, size, first, last);
+        quickSort(arr, size, first, mid-1);
+        quickSort(arr, size, mid, last);
+    }
+}
+
+void sorts::quick_sort(int* arr, ull size) {
+    ull first,last;
+    first = 0;
+    last = size;
+    quickSort(arr,size,first,last);
+}
+
--- p1/sorts.h
+++ p1/sorts.h
@@ -6,6 +6,12 @@ namespace sorts {
 
     void insertion_sort(int* arr, ull size);
 
+    void swap(int* arr, ull item1, ull item2);
+
+    void partition(int* arr, ull size , ull first, ull last);
+
+    void quickSort(int* arr, ull size, ull first, ull last);
+
     void quick_sort(int* arr, ull size);
 
 }
