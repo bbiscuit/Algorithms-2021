43a65667ba7bfa9a655df84c97f2a19ee00a568e
Andrew Huffman <ahuffman@cedarville.edu>
2021-10-04 17:29:44 -0400

    project 2 init

---
+++ com/algorithms/makingchange/ChangeMaker.java
@@ -0,0 +1,13 @@
+package com.algorithms.makingchange;
+
+/**
+ * Base class for all types of change making dynamic-programming algorithms.
+ * 
+ * @author Andrew Huffman
+ * @author Kyle Samuelson
+ */
+public class ChangeMaker {
+    
+
+
+}
