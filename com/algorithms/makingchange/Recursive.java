26c2c008fde6606e3466880ed429e60e075563be
yaboiksa <ky.samuel1@gmail.com>
2021-10-04 19:56:19 -0400

    Done Recursive?

---
+++ com/algorithms/makingchange/Recursive.java
@@ -0,0 +1,39 @@
+package com.algorithms.makingchange;
+
+public class Recursive implements ChangeMaker {
+    
+    @Override
+    public CoinPurse count(int value, DenominationSet set){
+        CoinPurse result = RecursiveSearch(value, set);
+        return result;
+    }
+
+
+    private CoinPurse RecursiveSearch(int value, DenominationSet set){
+        if(value == 0){
+            CoinPurse temp = new CoinPurse(set);
+            return temp;
+        }
+        CoinPurse bestCP = null;
+        int coinAdded = 0;
+        for (int i = 0; i < set.numDenominations(); i++){
+            int den = set.get(i);
+            int nextValue = (value - den);
+            if (nextValue > 0){
+                CoinPurse currCP = RecursiveSearch(nextValue, set);
+                if (bestCP == null) {
+                    bestCP = currCP;
+                    coinAdded = den;
+                }
+                if (currCP.getNumCoins() < bestCP.getNumCoins()) {
+                    bestCP = currCP;       
+                    coinAdded = den;     
+                }
+            }
+        }
+
+        bestCP.addCoin(coinAdded);
+        return bestCP;
+    }
+}
+
