5683daa00972793ecc4a70b2d52f228e37b61934
yaboiksa <ky.samuel1@gmail.com>
2021-10-05 18:49:29 -0400

    Changed test code to try a different denomination set and it does work with said denomination set.

--- com/algorithms/makingchange/TestFile.java
+++ com/algorithms/makingchange/TestFile.java
@@ -2,10 +2,11 @@ package com.algorithms.makingchange;
 
 public class TestFile {
     public static void main(String[] args) {
-        DenominationSet denom = new DenominationSet();
+        int[] denomSet = new int[]{1, 20};
+        DenominationSet denom = new DenominationSet(denomSet);
         CoinPurse testR = null;
         ChangeMaker testRecurse = new Recursive();
-        testR = testRecurse.count(50, denom);
+        testR = testRecurse.count(44, denom);
         System.out.print("Number of Coins back: ");
         System.out.println(testR.getNumCoins());
         for (int i = 0; i < denom.numDenominations(); i++){
