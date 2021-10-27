4225827bb0c25e37c3ddbbe235ba69d24dd636dc
Andrew Huffman <ahuffman@cedarville.edu>
2021-10-27 19:52:03 -0400

    Solution

--- com/algorithms/galacticbreakup/ProblemSolver.java
+++ com/algorithms/galacticbreakup/ProblemSolver.java
@@ -1,5 +1,7 @@
 package com.algorithms.galacticbreakup;
 
+import java.util.Scanner;
+
 public class ProblemSolver {
     private Galaxy galaxy;
     private ProblemDef problem;
@@ -12,6 +14,32 @@ public class ProblemSolver {
 
 
     public int solve() {
-        return 0;
+        for (int i = problem.getNumSteps() - 1; i >= 0; i--) {
+
+            for (int j = 0; j < problem.getNumDominionsInStep(i); j++) {
+                galaxy.formDominionExNihilo(
+                    new Dominion(problem.getDominion(i, j), 
+                        problem.getNDimension(), 
+                        problem.getMDimension(), 
+                        problem.getKDimension()
+                    )
+                );
+            }
+            
+        }
+
+        return galaxy.getMonthSeparated();
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int numProblems = s.nextInt();
+
+        for (int i = 0; i < numProblems; i++) {
+            ProblemDef pd = new ProblemDef(s);
+            ProblemSolver ps = new ProblemSolver(pd);
+            
+            System.out.println(ps.solve());
+        }
     }
 }
