bf5ecf22cb0b7a59191c09676af32da4d9222d06
Blue-Biscuit <ahuffman9000@gmail.com>
2021-10-27 23:39:37 -0400

    Custom exception for invalid disjoint set states

---
+++ com/algorithms/galacticbreakup/DisjointSetStateException.java
@@ -0,0 +1,7 @@
+package com.algorithms.galacticbreakup;
+
+public class DisjointSetStateException extends RuntimeException {
+    DisjointSetStateException(String str) {
+        super(str);
+    }
+}
