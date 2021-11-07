f9089512daad54b3d8051b229e2e91b90e6b5081
Chonky Default <ky.samuel1@gmail.com>
2021-11-06 21:42:39 -0400

    Added a toString for District and a displayBoard

--- com/algorithms/rushhour/District.java
+++ com/algorithms/rushhour/District.java
@@ -2,8 +2,29 @@ package com.algorithms.rushhour;
 
 public class District {
     private Coordinate[][] gameBoard;
+    private int[][] displayBoard;
+    private Vehicle goalVehicle;
 
-    District(){
+    public District(){
         gameBoard = new Coordinate[6][6];
+        // diplayBoard can be used for the algorithm to determine
+        // if it has checked this iteration
+        displayBoard = new int[6][6];
+    }
+
+
+    /**
+     * toString override of type District.
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 6; i++){
+            for (int j = 0; j < 6; j++){
+                sb.append(displayBoard[i][j] + " ");
+            }
+        }
+        return sb.toString();
     }
 }
