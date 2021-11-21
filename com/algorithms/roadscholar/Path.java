c90a9242b3abc4ad38a849755909a2a6d2400eea
Blue-Biscuit <ahuffman9000@gmail.com>
2021-11-21 15:55:55 -0500

    Path class instatiation

---
+++ com/algorithms/roadscholar/Path.java
@@ -0,0 +1,10 @@
+package com.algorithms.roadscholar;
+
+/**
+ * A path connecting two indexed nodes in the road-scholar problem.
+ * @author Andrew Huffman
+ */
+public class Path {
+    private int indexA;     // The index of the first node along the path.
+    private int indexB;     // The index of the second node along the path.
+}
