bea25c091fe5b24afcd819b4d4eeacafd6afb332
Blue-Biscuit <ahuffman9000@gmail.com>
2021-11-21 16:32:46 -0500

    ProblemDefinition class def.

---
+++ com/algorithms/roadscholar/ProblemDefinition.java
@@ -0,0 +1,12 @@
+package com.algorithms.roadscholar;
+
+/**
+ * Input reader class for the road-scholar problem.
+ * @author Andrew Huffman
+ */
+public class ProblemDefinition {
+    private int numCities;
+    private Node[] intersections;
+    private Path[] roads;
+    private Signpost[] signs;
+}
