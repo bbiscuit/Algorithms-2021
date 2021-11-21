c3ed8b70dd2507b752fb270780f27e1eda596b22
Blue-Biscuit <ahuffman9000@gmail.com>
2021-11-21 16:19:44 -0500

    Getters for Signpost data fields.

--- com/algorithms/roadscholar/Signpost.java
+++ com/algorithms/roadscholar/Signpost.java
@@ -8,4 +8,29 @@ public class Signpost {
     private int sourceIndex;        // The index of the source node.
     private float offset;           // The offset of the sign from the source.
     private int toIndex;            // The index of the node at the end of the path.
+
+    /**
+     * Gets the idnex of the source node.
+     * @return The source index.
+     */
+    public int getSource() {
+        return sourceIndex;
+    }
+
+    /**
+     * Gets the index of the node at the end of the path
+     * upon which this sign is found.
+     * @return The 'to' index.
+     */
+    public int getTo() {
+        return toIndex;
+    }
+
+    /**
+     * Gets how far the signpost is along the path it's on.
+     * @return The offset.
+     */
+    public float getOffset() {
+        return offset;
+    }
 }
