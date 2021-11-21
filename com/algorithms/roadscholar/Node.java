1e59babe67ef3c2031b072de649dde2edba3841d
Blue-Biscuit <ahuffman9000@gmail.com>
2021-11-21 15:32:25 -0500

    isCity() method on Node

--- com/algorithms/roadscholar/Node.java
+++ com/algorithms/roadscholar/Node.java
@@ -8,5 +8,11 @@ public class Node {
     private String name;        // The name of the string, or null if there's no city at this node.
     private int index;          // The index of the node; used in floyd-warshall.
 
-    
+    /**
+     * Whether or not the node is a city.
+     * @return True if the node is a city node.
+     */
+    public boolean isCity() {
+        return name != null;
+    }
 }
