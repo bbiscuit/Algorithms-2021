9f5de5f45739e48332c4259be171895317fe59a6
Blue-Biscuit <ahuffman9000@gmail.com>
2021-11-21 16:17:44 -0500

    Signpost class definition.

---
+++ com/algorithms/roadscholar/Signpost.java
@@ -0,0 +1,11 @@
+package com.algorithms.roadscholar;
+
+/**
+ * A signpost for the Road-scholar problem.
+ * @author Andrew Huffman
+ */
+public class Signpost {
+    private int sourceIndex;        // The index of the source node.
+    private float offset;           // The offset of the sign from the source.
+    private int toIndex;            // The index of the node at the end of the path.
+}
