b74c4b5c942eaacb1e4ca15d761942c314b8a8b1
Blue-Biscuit <ahuffman9000@gmail.com>
2021-11-21 16:22:47 -0500

    Signpost constructor.

--- com/algorithms/roadscholar/Signpost.java
+++ com/algorithms/roadscholar/Signpost.java
@@ -9,6 +9,19 @@ public class Signpost {
     private float offset;           // The offset of the sign from the source.
     private int toIndex;            // The index of the node at the end of the path.
 
+    public Signpost(int sourceIndex, int toIndex, float offset) {
+        Helpers.assertGreaterThanOrEqual(sourceIndex, 0, 
+            new IndexOutOfBoundsException("'sourceIndex' must be greater than 0. Val: " + sourceIndex));
+        Helpers.assertGreaterThanOrEqual(toIndex, 0, 
+            new IndexOutOfBoundsException("'toIndex' must be greater than 0. Val: " + toIndex));
+        Helpers.assertGreaterThanOrEqual(offset, 0.0f, 
+            new NegativeWeightException("Offset weight must be positive. Val: " + offset));
+        
+        this.sourceIndex = sourceIndex;
+        this.toIndex = toIndex;
+        this.offset = offset;
+    }
+
     /**
      * Gets the idnex of the source node.
      * @return The source index.
