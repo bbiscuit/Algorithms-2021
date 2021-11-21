ef7c7ac8213128b1a0c2082cca0a2ae02b87aef5
Blue-Biscuit <ahuffman9000@gmail.com>
2021-11-21 16:07:37 -0500

    Path constructor.

--- com/algorithms/roadscholar/Path.java
+++ com/algorithms/roadscholar/Path.java
@@ -9,6 +9,19 @@ public class Path {
     private int indexB;     // The index of the second node along the path.
     private float weight;   // The weight of the path.
 
+    public Path(int indexA, int indexB, float weight) {
+        Helpers.assertGreaterThanOrEqual(indexA, 0, 
+            new IndexOutOfBoundsException("'indexA' must be greater than or equal to 0. Val: " + indexA));
+        Helpers.assertGreaterThanOrEqual(indexB, 0, 
+            new IndexOutOfBoundsException("'indexB' must be greater than or equal to 0. Val : " + indexB));
+        Helpers.assertGreaterThanOrEqual(weight, 0.0f, 
+            new NegativeWeightException("Weights cannot be negative. Weight value: " + weight));
+        
+        this.indexA = indexA;
+        this.indexB = indexB;
+        this.weight = weight;
+    }
+
     /**
      * Gets the index for the first node along the path.
      * @return The index of the first node along the path.
